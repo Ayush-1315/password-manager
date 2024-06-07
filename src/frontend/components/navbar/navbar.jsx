a9cea7f063e4d527f1e5416026433bc3ae7ca20d
Ayush Raj <ayush.raj138@gmail.com>
2024-06-07 19:33:56 +0530

    style: added responsive navbar

---
+++ src/frontend/components/navbar/navbar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+import logo from "../../../assets/logo.ico";
+import navbarCSS from "./navbar.module.css";
+export const Navbar = () => {
+  return (
+    <div className={navbarCSS.navbar}>
+      <Link to="/">
+        <img src={logo} alt="Anzen-logo" className={navbarCSS.logo} />
+      </Link>
+      <div className={navbarCSS.navbarOptions}>
+        <span className={navbarCSS.features}>Features</span>
+        <div className={navbarCSS.startOptions}>
+          <Link to="/login">
+          <span className={`${navbarCSS.login} ${navbarCSS.authButtons}`}>
+            Login
+          </span>
+          </Link>
+          <Link to="/signup">
+          <span className={`${navbarCSS.signup} ${navbarCSS.authButtons}`}>
+            Signup
+          </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
