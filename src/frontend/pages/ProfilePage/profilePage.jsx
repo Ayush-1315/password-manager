896d0e503f93d357e6b4cea1052dba53fe3c11b1
Ayush Raj <ayush.raj138@gmail.com>
2024-07-11 01:33:43 +0530

    fix

--- src/frontend/pages/ProfilePage/profilePage.jsx
+++ src/frontend/pages/ProfilePage/profilePage.jsx
@@ -1,14 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../../context/authContext";
 import { editUserProfile, userProfile } from "../../services/userServices";
 import { ProfileCard } from "../../components/profileCard/profilecard";
 import { EditProfile } from "../../components/editProfile/editProfile";
 
 export const Profile = () => {
-  const { isLogin,logoffUser } = useAuth();
+  const { isLogin, logoffUser } = useAuth();
   const [userData, setUserData] = useState(null);
   const [editProfileForm, setEditProfileForm] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [userTimeout, setUserTimeout] = useState(false);
+  const [timer,setTimer]=useState(5);
+  const timerIdRef=useRef(null)
+
   useEffect(() => {
     (async () => {
       try {
@@ -19,14 +23,33 @@ export const Profile = () => {
           setUserData({ ...res?.data?.data?.user });
         }
       } catch (e) {
-        console.log(e);
         setUserData(false);
+        if (e?.response?.status === 401) {
+          setUserTimeout(true);
+
+          if (timerIdRef.current) {
+            clearInterval(timerIdRef.current);
+          }
+          timerIdRef.current=setInterval(()=>{
+              setTimer(prev=>{
+                console.log(`Timer value ${prev}`)
+                if(prev===0){
+                  clearInterval(timerIdRef.current);
+                  logoffUser();
+                  return 0
+                }
+                else{
+                  return prev-1
+                }
+              })
+          },1000)
+        }
       } finally {
         setIsLoading(false);
       }
     })();
     document.title = `Profile | Anzen`;
-  }, [isLogin]);
+  }, [isLogin,logoffUser]);
   const submitHandler = async (data) => {
     try {
       const id = isLogin?.user?.id;
@@ -50,32 +73,38 @@ export const Profile = () => {
     }
   };
 
-  const cancelHandler=()=>{
+  const cancelHandler = () => {
     setEditProfileForm(false);
-  }
+  };
   return (
     <>
-      {editProfileForm && userData && (
-        <EditProfile
-          userData={userData}
-          onSubmit={(data) => {
-            submitHandler(data);
-          }}
-          onCancel={cancelHandler}
-        />
-      )}
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : userData ? (
-        <ProfileCard
-          userData={userData}
-          onEdit={() => {
-            setEditProfileForm(true);
-          }}
-          onLogOff={()=>logoffUser()}
-        />
+      {userTimeout ? (
+        <span>Token Expired {timer}</span>
       ) : (
-        <span>Sorry for Inconvinience, there are some server issues.</span>
+        <div>
+          {editProfileForm && userData && (
+            <EditProfile
+              userData={userData}
+              onSubmit={(data) => {
+                submitHandler(data);
+              }}
+              onCancel={cancelHandler}
+            />
+          )}
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : userData ? (
+            <ProfileCard
+              userData={userData}
+              onEdit={() => {
+                setEditProfileForm(true);
+              }}
+              onLogOff={() => logoffUser()}
+            />
+          ) : (
+            <span>Sorry for Inconvinience, there are some server issues.</span>
+          )}
+        </div>
       )}
     </>
   );
